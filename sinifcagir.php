40c8c5d80e16cf7300486c1c4f3a766f5662e4b1
aysunkarahan <aysunnkarahan@gmail.com>
2015-07-24 16:58:06 +0300

    kelimeAyikla sınıfının kullanılması
    kelime_bul metodunun çağırılması

---
+++ sinifcagir.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'kelimeAyikla.php';
+$elma = new kelimeAyikla();
+$elma->kelime_bul("gök müydü dönmek için can atılacak taraf
+göktü evet gizlice göz kırptı öldürene
+göktü aynı gökyüzüydü ölene el altından
+tanışıklık veren de ","gök");?>
