f3dd2ced7e0f2c8f97b898009333c3d24376262a
aysunkarahan <aysunnkarahan@gmail.com>
2015-07-24 16:49:37 +0300

    bir metin içinde geçen bir kelimenin o metin içindeki sayısını bulma ve o kelimenin stilini değiştiren class:)
    bir metin içinde geçen bir kelimenin o metin içindeki sayısını bulma ve
    metnin içinde o kelimenin stilini değiştiren class:)

---
+++ kelimeAyikla.php
@@ -0,0 +1,20 @@
+<?php
+class kelimeAyikla{
+   function kelime_bul($metin,$aranan){
+       $dizi =explode(" ",$metin);
+       $i=0;
+       foreach($dizi as $val=>$key ){
+           if($key==$aranan){
+               $i++;
+          }
+       }
+       echo "Kelime olaraktan ".$i." tane ".$aranan." kelimesi var</br>";
+       $str = str_replace($aranan,'<span style="color:blue;text-align:center;text-decoration: underline;font-size: 200%;">'.$aranan.' </span>',$dizi);
+       $yeniden = implode(" ",$str);
+       //var_dump($str);
+       echo $yeniden;
+   }
+
+}
+
+?>
