f37edbd66e4bd2c32cce4a8237aecee536b27f1f
aysunkarahan <aysunnkarahan@gmail.com>
2015-07-26 02:58:57 +0300

    26.07.2015

---
+++ KelimeArama/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kelime Arama Uygulaması</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+</head>
+<body>
+
+<form action="" method="post">
+    <div class="form-group">
+        <label for="metin">Buraya Metin Giriniz</label>
+    <textarea class="form-control" name="metin"  cols="30" rows="10">
+
+    </textarea>
+        </div>
+    <div class="form-group">
+    <input type="text" class="form-control" name="aranan" placeholder="Aranacak Kelime."/>
+        </div>
+    <div class="form-group">
+    <input type="submit" class="form-control" value="ARA"/>
+        </div>
+</form>
+
+</body>
+</html>
+
+<?php
+require_once 'kelimeAyikla.php';
+$elma = new kelimeAyikla();
+if(isset($_POST["metin"])&&isset($_POST["aranan"])&&!empty($_POST["metin"])&&!empty($_POST["aranan"])){
+    $elma->kelime_bul($_POST["metin"], $_POST["aranan"]);
+}
+?>
---
+++ KelimeArama/kelimeAyikla.php
@@ -0,0 +1,21 @@
+<?php
+class kelimeAyikla{
+
+   function kelime_bul($metin,$aranan){
+       /*fonksiyondaki $metin parametresi metin $aranan parametresi ise o metinde aranacak kelimeyi ifade eder*/
+       $dizi =explode(" ",$metin);
+       $i=0;
+       foreach($dizi as $val=>$key ){
+           if($key==$aranan){
+               $i++;
+          }
+       }
+       echo "Kelime olaraktan ".$i." tane ".$aranan." kelimesi var</br>";
+       $str = str_replace($aranan,'<span style="color:blue;text-align:center;text-decoration: underline;font-size: 200%;">'.$aranan.' </span>',$dizi);
+       $yeniden = implode(" ",$str);
+       echo $yeniden;
+   }
+
+}
+
+?>
