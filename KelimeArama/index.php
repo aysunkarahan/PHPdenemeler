1687c408a8e28b3c0244b61978e17aa0ccf92f06
aysunkarahan <aysunnkarahan@gmail.com>
2015-07-26 03:12:52 +0300

    26.07.2015

--- KelimeArama/index.php
+++ KelimeArama/index.php
@@ -4,20 +4,31 @@
     <title>Kelime Arama Uygulaması</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <style>
+        .kutu{
+            margin: auto;
+            width: 500px;
+            border: 1px;
+        }
+    </style>
 </head>
 <body>
+<div class="kutu">
 <form action="" method="post">
     <div class="form-group">
-    <label for="metin">Buraya Metin Giriniz</label>
-    <textarea class="form-control" name="metin"  cols="30" rows="10"></textarea>
-    </div>
+        <label for="metin">Buraya Metin Giriniz</label>
+    <textarea class="form-control" name="metin"  cols="30" rows="10">
+
+    </textarea>
+        </div>
     <div class="form-group">
     <input type="text" class="form-control" name="aranan" placeholder="Aranacak Kelime."/>
-    </div>
+        </div>
     <div class="form-group">
     <input type="submit" class="form-control" value="ARA"/>
-    </div>
+        </div>
 </form>
+</div>
 </body>
 </html>
 
