caf49620db4c31471fab3cd84c9541c8828f2bb9
aysunkarahan <aysunnkarahan@gmail.com>
2015-07-26 03:15:39 +0300

    Update index.php

--- KelimeArama/index.php
+++ KelimeArama/index.php
@@ -16,17 +16,15 @@
 <div class="kutu">
 <form action="" method="post">
     <div class="form-group">
-        <label for="metin">Buraya Metin Giriniz</label>
-    <textarea class="form-control" name="metin"  cols="30" rows="10">
-
-    </textarea>
-        </div>
+    <label for="metin">Buraya Metin Giriniz</label>
+    <textarea class="form-control" name="metin"  cols="30" rows="10"></textarea>
+    </div>
     <div class="form-group">
     <input type="text" class="form-control" name="aranan" placeholder="Aranacak Kelime."/>
-        </div>
+    </div>
     <div class="form-group">
     <input type="submit" class="form-control" value="ARA"/>
-        </div>
+    </div>
 </form>
 </div>
 </body>
